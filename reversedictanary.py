99463f0ec84fc0403c50706a8d05f5cf8b11ba6b
lilcodehacker <arthurromneycook@gmail.com>
2025-06-02 17:46:54 -0400

    Update reversedictanary.py

--- reversedictanary.py
+++ reversedictanary.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
-from random_word import RandomWords
-from wordhoard import Definitions
-r = RandomWords()
-exit_conditions = (":q", "quit", "exit", "i'm done")
+from wonderwords import RandomWord
+from dictionary.britannica import get_definitions
+r = RandomWord()
+points = 0
+exit_conditions = (":q", "quit", "exit", "i'm done", "i quit")
 while True:
-    word = r.get_random_word()
-    definition = Definitions(search_string=word)
-    definition_results = definition.find_definitions()
-    print(f"📚{definition_results}📚")
+    word = r.word()
+    list = get_definitions(word)
+    string = str(list)
+    string = string.replace(word, "")
+    print(f"{string}")
     while True:
         query = input("What is the Word?📚 ")
         if query in exit_conditions:
             print ("You quitter👎 the Word was: " + word + "!!")
+            if points > 10:
+                print (f"Your score was {points}!!👍")
+            else:
+                print (f"Your score was {points}👎")
             quit()
         else:
-            if query in word:
+            if query == word:
                 print (f"You Did it👍 the Word was: {word}!!")
                 break
             else:
                 print (f"WRONG!!!!")
+    points = points+1
